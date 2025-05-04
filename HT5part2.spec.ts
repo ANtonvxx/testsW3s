073a496d01b84a6aa498a2b4abe87ad66a8e8b57
Anton <testboxantonx@gmail.com>
2025-05-04 15:51:49 +0300

    HT5, part2, tests reworked according to task

--- HT5part2.spec.ts
+++ HT5part2.spec.ts
@@ -14,17 +14,34 @@ test.beforeEach(async ({page}) => {
     await page.goto('http://uitestingplayground.com/autowait'); 
 })
 
-test ("pageLocatorWaitFor", async ({page}) => {
-   // await page.locator('div.btn btn-primary').waitFor({state: 'visible'});
-    await page.locator('div.col-6').getByRole('button',{name:'Button'}).click();
-})
 
+test ("checkingInput", async ({page}) =>{
+    //choose 'Input' value from the dropdown
+    await page.getByLabel ("Choose an element type").selectOption('input');   
 
-test ("pageWaitForSelector", async ({page}) => {
-    await page.waitForSelector('button#target')
-    await page.locator('button#target').click();
-})
-    
-test ("waitForTimeout", async ({page}) => {
-    await page.waitForTimeout(7000)
+    //Mark/unmark a bunch of checkboxes with setting
+    await page.getByText ('Visible').check();
+    await page.getByText ('Enabled').check();
+    await page.locator ('.form-check-label').getByText('Editable').uncheck();
+    await page.getByText ('On Top').uncheck();
+    await page.getByText ('Non Zero Size').uncheck(); 
+
+    //Click the 'Apply for 10 seconds' button
+    await page.locator('#applyButton10').click();
+
+    //Assertion that Input is readOnly (is not Editable)
+    await expect (page.locator('#target')).toHaveAttribute('readonly');
+
+    //Assertion that Input is Zero size   
+    await expect (page.locator('#target')).toHaveCSS('width', '0px');
+
+    //Show the actual size of #target input in console log
+    //console.log (await page.locator('#target').evaluate((el) => getComputedStyle(el).width)); //0px
+
+    //Assertion that text was entered (enter value to the Input and check that it was correctly entered)
+    await page.locator ('#target').fill('Some test text');
+    await expect (page.locator('#target')).toHaveValue('Some test text');
+//    await page.getByRole('heading', { name: 'Playground' }).locator ('#target').fill('Some test text'); - why doesn't this work? (with getByRole)    
+
+    await page.waitForTimeout (1000);
 })
